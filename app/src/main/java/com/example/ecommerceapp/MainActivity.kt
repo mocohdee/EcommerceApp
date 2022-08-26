93ae36c6212aa2e553f2fbcf2eef3cfb1e8895c2
Diana Mocoh <mocohdiana04@gmail.com>
2022-08-26 12:02:35 +0300

    added a splashscreen

---
+++ app/src/main/java/com/example/ecommerceapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ecommerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//View Group - is an invisible container of other views.
+//Types of view Group ->child view, ->view Group
---
+++ app/src/main/java/com/example/ecommerceapp/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerceapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerceapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
